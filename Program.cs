d34057ca2faf7855609f259da7a1f8a73b88b466
RodrigoCSbegue <rcsbegue@gmail.com>
2026-04-24 15:27:38 -0300

    adicionando add controller

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
